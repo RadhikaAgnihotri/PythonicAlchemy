9a3e2c4d6f83cd1168cc871e644180125f49a1f6
RadhikaAgnihotri <radhikaagnihotri038@gmail.com>
2024-02-22 23:43:40 +0530

    Pattern Pizzazz

--- I_Love_Patterns!.py
+++ I_Love_Patterns!.py
@@ -341,6 +341,37 @@ for i in range(n-1):
     print(' '*(i+1)+'* '*(n-i-1))
 print()
        
+#----------x----------x----------x----------x----------x----------x 
+
+#To print a diamond, if n=4
+#    1
+#   2 2
+#  3 3 3
+# 4 4 4 4
+#  3 3 3
+#   2 2
+#    1
+
+#1st method
+n = (int)(input("Enter a number :"))
+for i in range(n): #1st half of diamond i.e. pyramid
+    print(' '*(n-i-1),end='')
+    for j in range(i+1):
+        print((i+1),end=' ')
+    print()    
+for i in range(n-1): #2nd half of diamond i.e. inverted pyramid
+    print(' '*(i+1),end='')
+    for j in range(n-i-1):
+        print((n-i-1),end=' ')
+    print()    
+  
+#2nd method/concise
+n = (int)(input("Enter a number :"))
+for i in range(n):
+    print(' '*(n-i-1)+(str(i+1)+' ')*(i+1))
+for i in range(n-1):
+    print(' '*(i+1)+(str(n-i-1)+' ')*(n-i-1))
+          
 #----------x----------x----------x----------x----------x----------x       
       
         
