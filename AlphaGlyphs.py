239eab8e9e0c2670600926f0e00656b24fa30e05
RadhikaAgnihotri <radhikaagnihotri038@gmail.com>
2024-02-27 21:15:19 +0530

    Lexical Glyph Conjuring!

--- AlphaGlyphs.py
+++ AlphaGlyphs.py
@@ -1,5 +1,3 @@
-#The AlphaGlyphs
-
 #To print the alphabet A using '*' symbol
 
 #  ***
@@ -10,13 +8,27 @@
 # *   *
 # *   *
 
-n=7
+#Brute force approach
+n = 7
 for i in range(n):
     if i == 0:
-        print(' '+'*'*3)
+        print('  '+'* '*3)
     if i == 1 or i == 2:
-        print('*'+' '*3+'*')
+        print('* '+'  '*3+'* ')
     if i == 3:
-        print('*'*5)
+        print('* '*5)
     if i == 4 or i == 5 or i == 6:
-        print('*'+' '*3+'*')
+        print('* '+'  '*3+'* ')
+        
+#Systematic, thought through approach    
+for row in range(7):
+    for col in range(5):
+        if (row == 0) and (col in {1,2,3}):
+            print('*',end=' ')
+        elif (row in {1,2,4,5,6}) and (col in {0,4}):
+            print('*',end=' ')
+        elif (row == 3):
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print() #since next row will start in new line          
