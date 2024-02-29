a32e38025f12ab1d5ce154ddd27adca756894414
RadhikaAgnihotri <radhikaagnihotri038@gmail.com>
2024-02-29 18:42:18 +0530

    Lexical Glyph Conjuring!

--- AlphaGlyphs.py
+++ AlphaGlyphs.py
@@ -114,4 +114,26 @@ for row in range(7):
     print()        
     
 #----------x----------x----------x----------x----------x----------x----------x----------
+
+#To print the alphabet C using '*' symbol
+
+# * * * * *
+# *       
+# *       
+# * * * * *      
+# *  
+# *
+# * * * * *
+
+for row in range(7):
+    for col in range (5):
+        if row in {0,3,6}:
+            print('*',end = ' ')
+        elif row in {1,2,4,5} and col == 0:
+            print('*',end = ' ')
+        else:
+            print(' ',end=' ')
+    print() 
+    
+#----------x----------x----------x----------x----------x----------x----------x----------
         
--- Anagrams.py
+++ Anagrams.py
@@ -8,7 +8,7 @@
 #Solution from scratch
 word_1 = input("Please enter the 1st word :")
 word_2 = input("Please enter the 2nd word :")
-new_word_1 = word_1.lower()
+new_word_1 = word_1.lower() #to handle case sensitivity
 new_word_2 = word_2.lower()
 if (sorted(new_word_1) == sorted(new_word_2)):
     print(word_1,"is a valid anagram of" ,word_2, "or vice versa")
