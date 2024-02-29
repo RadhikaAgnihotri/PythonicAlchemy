489e27f56053b157a222ec876cefb15a71bd56a5
RadhikaAgnihotri <radhikaagnihotri038@gmail.com>
2024-02-29 18:44:22 +0530

    Word Jumble Judge

--- Anagrams.py
+++ Anagrams.py
@@ -9,7 +9,7 @@
 word_1 = input("Please enter the 1st word :")
 word_2 = input("Please enter the 2nd word :")
 new_word_1 = word_1.lower() #to handle case sensitivity
-new_word_2 = word_2.lower()
+new_word_2 = word_2.lower() #to convert all letters in lowercase then compare
 if (sorted(new_word_1) == sorted(new_word_2)):
     print(word_1,"is a valid anagram of" ,word_2, "or vice versa")
 else:
