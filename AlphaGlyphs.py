4529501f4eeb0518656baeb7c23387f3ec81448a
RadhikaAgnihotri <radhikaagnihotri038@gmail.com>
2024-03-04 19:28:21 +0530

    Lexical Glyph Conjuring!

--- AlphaGlyphs.py
+++ AlphaGlyphs.py
@@ -228,4 +228,30 @@ for row in range(7):
     print() 
     
 #----------x----------x----------x----------x----------x----------x----------x----------
+
+#To print the letter J using '*' symbol
+
+# * * * * *       
+#     *       
+#     *       
+#     *     
+#     *       
+# *   *       
+#   *        
+
+for row in range(7):
+    for col in range (5):
+        if row == 0:
+            print('*',end = ' ')
+        elif row not in  {0,5,6} and col == 2:
+            print('*',end =' ')
+        elif row == 5 and col in {0,2}:
+            print('*',end =' ')  
+        elif row == 6 and col == 1:
+            print('*',end =' ')    
+        else:
+            print(' ',end=' ')
+    print() 
+    
+#----------x----------x----------x----------x----------x----------x----------x----------
         
