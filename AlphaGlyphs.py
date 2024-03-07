62021bc77c256845c31d62e914e0ec01cfb08176
RadhikaAgnihotri <radhikaagnihotri038@gmail.com>
2024-03-07 19:19:05 +0530

    Lexical Glyph Conjuring!

--- AlphaGlyphs.py
+++ AlphaGlyphs.py
@@ -280,4 +280,26 @@ for row in range(7):
     print() 
     
 #----------x----------x----------x----------x----------x----------x----------x----------
+
+#To print the letter L using '*' symbol
+
+# *          
+# *     
+# *      
+# *   
+# *  
+# *        
+# * * * * *     
+
+for row in range(7):
+    for col in range (5):
+        if row != 6 and col == 0:
+            print('*',end = ' ')
+        elif row == 6:
+            print('*',end =' ')    
+        else:
+            print(' ',end=' ')
+    print() 
+    
+#----------x----------x----------x----------x----------x----------x----------x----------
         
