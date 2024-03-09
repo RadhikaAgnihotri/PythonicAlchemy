6f42b3b486a5da62d2791aa384e81419a71f22e9
RadhikaAgnihotri <radhikaagnihotri038@gmail.com>
2024-03-09 17:26:47 +0530

    Lexical Glyph Conjuring!

--- AlphaGlyphs.py
+++ AlphaGlyphs.py
@@ -326,4 +326,31 @@ for row in range(7):
     print() 
     
 #----------x----------x----------x----------x----------x----------x----------x----------
+
+#To print the letter N using '*' symbol
+
+# *         *     
+# * *       *
+# *   *     *     
+# *     *   *     
+# *       * *
+# *         *  
+           
+for row in range(6):
+    for col in range (6):
+        if col in {0,5}:
+            print('*',end = ' ')
+        elif row == 1 and col == 1:
+            print('*',end =' ')
+        elif row == 2 and col == 2:
+            print('*', end = ' ')
+        elif row == 3 and col == 3:
+            print('*', end = ' ')
+        elif row == 4 and col == 4:
+            print('*', end = ' ')    
+        else:
+            print(' ',end=' ')
+    print() 
+    
+#----------x----------x----------x----------x----------x----------x----------x----------
         
