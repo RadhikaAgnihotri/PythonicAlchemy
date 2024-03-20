8f0110b1c87e0ff42bf336594da43edfa32bfde3
RadhikaAgnihotri <radhikaagnihotri038@gmail.com>
2024-03-20 21:56:55 +0530

    Lexical Glyph Conjuring!

--- AlphaGlyphs.py
+++ AlphaGlyphs.py
@@ -399,4 +399,35 @@ for row in range(7):
     print()
 
 #----------x----------x----------x----------x----------x----------x----------x----------
+
+#To print the letter Q using '*' symbol
+
+#   * * *           
+# *       *
+# *       *     
+# *       *
+# *   *   *  
+# *     * *  
+#   * * * * 
+#           *
+           
+for row in range(8):
+    for col in range (6):
+        if row in {1,2,3} and col in {0,4}:
+            print('*',end = ' ')
+        elif row == 0 and col in {1,2,3}:
+            print('*',end =' ')
+        elif row == 4 and col in {0,2,4}:
+            print('*', end = ' ')
+        elif row == 5 and col in {0,3,4}:
+            print('*', end = ' ') 
+        elif row == 6 and col in {1,2,3,4}:
+            print('*', end = ' ') 
+        elif row == 7 and col == 5:
+            print('*', end = ' ')    
+        else:
+            print(' ',end=' ')
+    print() 
+    
+#----------x----------x----------x----------x----------x----------x----------x----------
         
