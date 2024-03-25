0fdf27a54e7cbfe05616e1c3fc8380e84cb80843
RadhikaAgnihotri <radhikaagnihotri038@gmail.com>
2024-03-25 18:52:40 +0530

    Lexical Glyph Conjuring!

--- AlphaGlyphs.py
+++ AlphaGlyphs.py
@@ -506,4 +506,26 @@ for row in range(7):
     print() 
     
 #----------x----------x----------x----------x----------x----------x----------x----------
+
+#To print the letter U using '*' symbol
+
+# *       *            
+# *       *      
+# *       *            
+# *       *  
+# *       *   
+# *       *    
+#   * * *      
+           
+for row in range(7):
+    for col in range (5):
+        if row != 6 and col in {0,4}:
+            print('*',end = ' ')
+        elif row == 6 and col in {1,2,3}:
+            print('*',end =' ')
+        else:
+            print(' ',end=' ')
+    print() 
+
+#----------x----------x----------x----------x----------x----------x----------x----------
         
