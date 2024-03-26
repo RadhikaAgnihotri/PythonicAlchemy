c2327938d4ef040960a06f8667a9cccd1efbf2c2
RadhikaAgnihotri <radhikaagnihotri038@gmail.com>
2024-03-26 14:53:28 +0530

    Lexical Glyph Conjuring!

--- AlphaGlyphs.py
+++ AlphaGlyphs.py
@@ -528,4 +528,36 @@ for row in range(7):
     print() 
 
 #----------x----------x----------x----------x----------x----------x----------x----------
+
+#To print the letter V using '*' symbol
+
+# *           *            
+#  *         *      
+#   *       *            
+#    *     *  
+#     *   *   
+#      * *    
+#       *      
+           
+for row in range(7):
+    for col in range (13):
+        if row == 0 and col in {0,12}:
+            print('*',end = '')
+        elif row == 1 and col in {1,11}:
+            print('*',end ='')
+        elif row == 2 and col in {2,10}:
+            print('*',end ='') 
+        elif row == 3 and col in {3,9}:
+            print('*',end ='')
+        elif row == 4 and col in {4,8}:
+            print('*',end ='')
+        elif row == 5 and col in {5,7}:
+            print('*',end ='') 
+        elif row == 6 and col == 6:
+            print('*',end ='')    
+        else:
+            print(' ',end='')
+    print() 
+
+#----------x----------x----------x----------x----------x----------x----------x----------
         
