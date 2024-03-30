07d2c94e64fa32149f47f2ea5ce374204cdeb5ff
RadhikaAgnihotri <radhikaagnihotri038@gmail.com>
2024-03-30 20:15:03 +0530

    Lexical Glyph Conjuring!

--- AlphaGlyphs.py
+++ AlphaGlyphs.py
@@ -636,4 +636,34 @@ for row in range(7):
     print() 
 
 #----------x----------x----------x----------x----------x----------x----------x----------
+
+#To print the letter Z using '*' symbol
+
+# * * * *           
+#      *       
+#     *         
+#    *         
+#   *  
+#  *     
+# * * * *     
+           
+for row in range(7):
+    for col in range (7):
+        if row in {0,6} and col in {0,2,4,6}:
+            print('*',end = '')
+        elif row == 1 and col == 5:
+            print('*',end ='')
+        elif row == 2 and col == 4:
+            print('*',end ='')
+        elif row == 3 and col == 3:
+            print('*',end ='')
+        elif row == 4 and col == 2:
+            print('*',end ='') 
+        elif row == 5 and col == 1:
+            print('*',end ='')    
+        else:
+            print('',end=' ')
+    print()
+    
+#----------x----------x----------x----------x----------x----------x----------x----------
         
